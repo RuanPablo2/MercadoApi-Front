74ea4de481b9cda0ddf3fb61005920ddaaf6a165
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2025-04-23 23:07:36 -0300

    home products adjustments

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,13 +10,25 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
   featuredProducts = [
-    { name: 'Arroz Orgânico', price: 15.90, image: 'assets/products/arroz.jpg' },
-    { name: 'Feijão Preto', price: 7.50, image: 'assets/products/feijao.jpg' },
-    { name: 'Óleo de Girassol', price: 10.00, image: 'assets/products/oleo.jpg' },
-    { name: 'Arroz Orgânico', price: 15.90, image: 'assets/products/arroz.jpg' },
-    { name: 'Feijão Preto', price: 7.50, image: 'assets/products/feijao.jpg' },
-    { name: 'Óleo de Girassol', price: 10.00, image: 'assets/products/oleo.jpg' },
-    { name: 'Arroz Orgânico', price: 15.90, image: 'assets/products/arroz.jpg' },
-    { name: 'Feijão Preto', price: 7.50, image: 'assets/products/feijao.jpg' }
+    { name: 'Arroz Orgânico', price: 15.90, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/193571/906417_1.jpg?v=637272430399600000' },
+    { name: 'Feijão Preto', price: 7.50, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/55790288/feijao-preto-tipo-1-namorado-1kg-1.jpg?v=637892510712800000' },
+    { name: 'Óleo de Girassol', price: 10.00, image: 'https://cdn.awsli.com.br/2500x2500/447/447503/produto/144287272/6d3e510dd7.jpg' },
+    { name: 'Arroz Orgânico', price: 15.90, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/193571/906417_1.jpg?v=637272430399600000' },
+    { name: 'Feijão Preto', price: 7.50, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/55790288/feijao-preto-tipo-1-namorado-1kg-1.jpg?v=637892510712800000' },
+    { name: 'Óleo de Girassol', price: 10.00, image: 'https://cdn.awsli.com.br/2500x2500/447/447503/produto/144287272/6d3e510dd7.jpg' },
+    { name: 'Arroz Orgânico', price: 15.90, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/193571/906417_1.jpg?v=637272430399600000' },
+    { name: 'Feijão Preto', price: 7.50, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/55790288/feijao-preto-tipo-1-namorado-1kg-1.jpg?v=637892510712800000' },
+    { name: 'Arroz Orgânico', price: 15.90, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/193571/906417_1.jpg?v=637272430399600000' },
+    { name: 'Feijão Preto', price: 7.50, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/55790288/feijao-preto-tipo-1-namorado-1kg-1.jpg?v=637892510712800000' },
+    { name: 'Arroz Orgânico', price: 15.90, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/193571/906417_1.jpg?v=637272430399600000' },
+    { name: 'Feijão Preto', price: 7.50, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/55790288/feijao-preto-tipo-1-namorado-1kg-1.jpg?v=637892510712800000' },
+    { name: 'Óleo de Girassol', price: 10.00, image: 'https://cdn.awsli.com.br/2500x2500/447/447503/produto/144287272/6d3e510dd7.jpg' },
+    { name: 'Arroz Orgânico', price: 15.90, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/193571/906417_1.jpg?v=637272430399600000' },
+    { name: 'Feijão Preto', price: 7.50, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/55790288/feijao-preto-tipo-1-namorado-1kg-1.jpg?v=637892510712800000' },
+    { name: 'Óleo de Girassol', price: 10.00, image: 'https://cdn.awsli.com.br/2500x2500/447/447503/produto/144287272/6d3e510dd7.jpg' },
+    { name: 'Arroz Orgânico', price: 15.90, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/193571/906417_1.jpg?v=637272430399600000' },
+    { name: 'Feijão Preto', price: 7.50, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/55790288/feijao-preto-tipo-1-namorado-1kg-1.jpg?v=637892510712800000' },
+    { name: 'Arroz Orgânico', price: 15.90, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/193571/906417_1.jpg?v=637272430399600000' },
+    { name: 'Feijão Preto', price: 7.50, image: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/55790288/feijao-preto-tipo-1-namorado-1kg-1.jpg?v=637892510712800000' }
   ];
 }
