753724f7f37c77a03e94bb089426c2c3c3b7d85b
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2025-05-01 22:35:04 -0300

    Order and order-details route

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderService } from '../../services/order.service';
 import { OrderResponse } from '../../dto/response/order-response';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-orders',
@@ -13,12 +14,18 @@ import { OrderResponse } from '../../dto/response/order-response';
 export class OrdersComponent implements OnInit {
   orders: OrderResponse[] = [];
 
-  constructor(private orderService: OrderService) {}
+  constructor(private orderService: OrderService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.orderService.getAllOrders().subscribe({
       next: (data) => (this.orders = data),
       error: (err) => console.error('Erro ao buscar pedidos', err)
     });
+  }
+
+  goToDetails(orderId: number): void {
+    this.router.navigate(['/orders', orderId]);
   }
 }
