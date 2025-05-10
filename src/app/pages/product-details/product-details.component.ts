41e033d62f826b75dd95ce251311a7ee3854adff
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2025-05-10 00:59:27 -0300

    Product details

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatSnackBarModule],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.scss'
+  styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: any;
+  quantity = 1;
+  orderId!: number;
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private cartService: CartService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {
+  const productId = +this.route.snapshot.paramMap.get('id')!;
+  if (productId) {
+    this.productService.getProductById(productId).subscribe(data => {
+      this.product = data;
+    });
+  }
+
+  this.cartService.getCart().subscribe(cart => {
+    this.orderId = cart.id;
+  });
+}
+
+  increaseQuantity() {
+    if (this.product && this.quantity < this.product.availableQuantity) {
+      this.quantity++;
+    }
+  }
+
+  decreaseQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
+  addToCart() {
+    if (!this.orderId || !this.product) return;
+
+    const item = {
+      productId: this.product.id,
+      quantity: this.quantity
+    };
+
+    this.cartService.addItemToCart(this.orderId, item).subscribe({
+      next: () => {
+        this.snackBar.open('Produto adicionado ao carrinho!', '', { duration: 2000 });
+      },
+      error: () => {
+        this.snackBar.open('Erro ao adicionar produto ao carrinho', 'Fechar', { duration: 3000, panelClass: ['snackbar-error'] });
+      }
+    });
+  }
 }
